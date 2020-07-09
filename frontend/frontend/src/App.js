153482d9360ffad645bff1f0bbab5a52b798e13d
Chinmay-395 <chinmaydali395@gmail.com>
2020-07-10 00:38:52 +0530

    Added a header component

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route, Redirect, withRouter, BrowserRouter } from 'react-router-dom'
-
+import './app.css'
 import Home from './components/HomeComponent'
+import Header from './components/Header'
 function App() {
   return (
     <div>
-      {/* <Header/> */}
+      <Header />
       <BrowserRouter>
         <Switch>
           <Route path='/home' component={() => <Home />} />
--- frontend/frontend/src/components/HomeComponent.js
+++ frontend/frontend/src/components/HomeComponent.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import img1 from '../shared/img11.jpg'
+import img2 from '../shared/img13.jpg'
+import img3 from '../shared/img15.jpg'
 
 let getData = async () =>
     await axios.get(`http://127.0.0.1:8000/gpuApi/`)
@@ -11,23 +14,62 @@ class Home extends Component {
 
     handleClick = () => {
         console.log("Clicked")
-
         getData()
     }
     render() {
         return (
-            <div>
-                <h1>Home Page</h1>
-                <button onClick={this.handleClick}>Click me</button>
-            </div>
+            <>
+                <CarouselFuncComponent />
+                <div className="container">
+                    <div className="row">
+                        <h1>Home Page</h1>
+                        <br />
+                        <button onClick={this.handleClick}>Click me</button>
+                    </div>
+                </div>
+            </>
         )
     }
 }
 export default Home;
 
-// let getData = async () =>
-//     await axios.get(`http://127.0.0.1:8000/gpuApi/`)
-//         .then(response => console.log(response.data))
-//         .catch(error => console.log(error))
-
-// getData()
+export const CarouselFuncComponent = () => {
+    return (
+        <div id="demo" className="carousel slide" data-ride="carousel">
+            <ul className="carousel-indicators">
+                <li data-target="#demo" data-slide-to="0" className="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+            </ul>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src={img1} alt="Los Angeles" width="1100" height="500" />
+                    <div className="carousel-caption">
+                        <h3>Los Angeles</h3>
+                        <p>We had such a great time in LA!</p>
+                    </div>
+                </div>
+                <div className="carousel-item">
+                    <img src={img3} alt="Chicago" width="1100" height="500" />
+                    <div className="carousel-caption">
+                        <h3>Chicago</h3>
+                        <p>Thank you, Chicago!</p>
+                    </div>
+                </div>
+                <div className="carousel-item">
+                    <img src={img2} alt="New York" width="1100" height="500" />
+                    <div className="carousel-caption">
+                        <h3>New York</h3>
+                        <p>We love the Big Apple!</p>
+                    </div>
+                </div>
+            </div>
+            <a className="carousel-control-prev" href="#demo" data-slide="prev">
+                <span className="carousel-control-prev-icon"></span>
+            </a>
+            <a className="carousel-control-next" href="#demo" data-slide="next">
+                <span className="carousel-control-next-icon"></span>
+            </a>
+        </div>
+    )
+}
